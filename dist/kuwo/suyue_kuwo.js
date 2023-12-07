2c5d4d04d371df2df3c384e605863e2a1f227c94
gtt <vale_gtt@163.com>
2023-12-07 10:56:58 +0800

    更新源

--- dist/kuwo/suyue_kuwo.js
+++ dist/kuwo/suyue_kuwo.js
@@ -73,7 +73,7 @@ async function getMediaSourceJxcxin(musicId) {
     const servercontent = (await (0, axios_1.default)({
         url: desUrl,
         method: 'get',
-        timeout: 100,
+        timeout: 1000,
     })).data;
     console.log(servercontent);
     if (servercontent.code == 200) {
