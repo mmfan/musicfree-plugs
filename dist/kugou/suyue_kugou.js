52431fb1acca462ad4fbcd75c274e7b477c73397
gtt <vale_gtt@163.com>
2023-12-08 17:07:55 +0800

    更新源

--- dist/kugou/suyue_kugou.js
+++ dist/kugou/suyue_kugou.js
@@ -347,7 +347,8 @@ module.exports = {
     version: "0.1.3",
     appVersion: ">0.1.0-alpha.0",
     order: 17,
-    srcUrl: "http://adad23u.appinstall.life/dist/kugou/index.js",
+    // srcUrl: "http://adad23u.appinstall.life/dist/kugou/index.js",
+    srcUrl: "https://agit.ai/vale_gtt/MSC_API/raw/branch/master/dist/kugou/suyue_kugou.js",
     cacheControl: "no-cache",
     primaryKey: ["id", "album_id", "album_audio_id"],
     hints: {
