c8fdf61547da8da3b334a2710e2737a46c53b6e6
gtt <vale_gtt@163.com>
2024-01-04 09:48:13 +0800

    增加解析元

--- my_plugins/kuwo/my_2t58.js
+++ my_plugins/kuwo/my_2t58.js
@@ -14,7 +14,7 @@ let enable_plugin = true;
 const pageSize = 30;
 
 async function get_plugin_token() {
-    const raw_html = (await axios_1.default.get(token_host + "/vale_gtt/MSC_API/raw/branch/master/my_plugins/token")).data
+    let raw_html = (await axios_1.default.get(token_host + "/vale_gtt/MSC_API/raw/branch/master/my_plugins/token")).data
     console.log("raw_html=",raw_html)
     if(raw_html === token_txt)
     {
