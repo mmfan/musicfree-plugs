fe5d245b4e44d5143b14bf6701cc288883fb903e
gtt <vale_gtt@163.com>
2024-01-04 09:48:54 +0800

    增加解析元

--- my_plugins/kuwo/my_2t58.js
+++ my_plugins/kuwo/my_2t58.js
@@ -16,15 +16,16 @@ const pageSize = 30;
 async function get_plugin_token() {
     let raw_html = (await axios_1.default.get(token_host + "/vale_gtt/MSC_API/raw/branch/master/my_plugins/token")).data
     console.log("raw_html=",raw_html)
-    if(raw_html === token_txt)
-    {
-        enable_plugin = true;
-        console.log("Token有效, 已使能本插件.")
-    }
-    else
+    if(raw_html != token_txt)
     {
         enable_plugin = false;
         console.log("Token无效, 本插件已禁用.")
+        
+    }
+    else
+    {
+        enable_plugin = true;
+        console.log("Token有效, 已使能本插件.")
     }
 }
 
