b774d7d5dcd1c6a0bc7245c6df0cb5e462039349
gtt <vale_gtt@163.com>
2024-01-04 08:38:57 +0800

    增加解析元

--- my_plugins/kuwo/my_2t58.js
+++ my_plugins/kuwo/my_2t58.js
@@ -288,7 +288,7 @@ module.exports = {
     version: "0.1.14",
     appVersion: ">0.1.0-alpha.0",
     order: 19,
-    // srcUrl: "https://agit.ai/vale_gtt/MSC_API/raw/branch/master/my_plugins/kuwo/my_2t58.js",
+    srcUrl: "https://agit.ai/vale_gtt/MSC_API/raw/branch/master/my_plugins/kuwo/my_2t58.js",
     cacheControl: "no-cache",
     hints: {
         importMusicSheet: [],
